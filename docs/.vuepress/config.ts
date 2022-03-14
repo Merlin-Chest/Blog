e243da32582ce528d7fee36525b8cc78920cf334
鸭脖yaboo <863176846@qq.com>
2022-03-14 19:55:01 +0800

    chore: use vuepress

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,25 +1,14 @@
 import AutoNavPlugin from 'vuepress-plugin-auto-navbar';
 
-const navbar = AutoNavPlugin({
-  subNavShow: ['其他', '工具使用', 'Vue', '设计模式', '前端工程化', '计算机网络', '算法基础', '刷题技巧'],
-  ignoreFolders: ["node_modules", "assets", "public", ".vuepress", "code", ".obsidian", "utils"], // 需要排除的一些目录
-})
-
 module.exports = {
   lang: 'zh-cn',
   title: 'Code More Create',
-  // 在使用 vuepress-vite 包的时候，你可以忽略这个字段，因为 Vite 是默认打包工具
-  bundler: '@vuepress/bundler-vite',
-  // Vite 打包工具的配置项
-  bundlerConfig: {
-    // 查看下方
-  },
   themeConfig: {
     logo: 'logo.jpg',
-    navbar,// 自动生成导航栏配置
-    sidebar: {
-      '/': navbar
-    },
+    navbar: AutoNavPlugin({
+      subNavShow: ['其他', '工具使用', 'Vue', '设计模式', '前端工程化', '计算机网络', '算法基础', '刷题技巧'],
+      ignoreFolders: ["node_modules", "assets", "public", ".vuepress", "code", ".obsidian", "utils"], // 需要排除的一些目录
+    }),// 自动生成导航栏配置
     // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
     displayAllHeaders: true,
     lastUpdated: true, // string | boolean
