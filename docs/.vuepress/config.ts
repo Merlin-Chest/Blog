742eab4b3d54ab62d192d110c435190eb6dada53
鸭脖yaboo <863176846@qq.com>
2022-05-22 16:49:14 +0800

    chore: serverWorker

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -5,15 +5,25 @@ import { pwaPlugin } from '@vuepress/plugin-pwa'
 
 module.exports = defineConfig({
   title: 'Code More Create',
+  description:"Merlin's Blog",
   plugins: [
     ['vuepress-plugin-auto-sidebar', {}],
-    ['@vuepress/plugin-pwa',{}]
+    ['@vuepress/plugin-pwa',{
+      serviceWorker:true,
+      updatePopup:true,
+    }]
   ],
   head:[
-    ['meta',{charset:'UTF-8'}],
-    ['meta',{httpEquiv:'X-UA-Compatible',content:'IE=edge'}],
-    ['meta',{name:'viewport',content:'width=device-width, initial-scale=1.0'}],
+    ['link', { rel: 'icon', href: '/logo.png' }],
     ['link', { rel: 'manifest', href: '/manifest.webmanifest' }],
+    ['meta',{name:'viewport',content:'width=device-width, initial-scale=1.0'}],
+    ['meta', { name: 'theme-color', content: '#4c58bb' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' }],
+    ['link', { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#4c58bb' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/mstile-150x150.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }],
   ],
   theme: 'vt',
   themeConfig: {
