0213a0771b0e07793b2a5d86efa09e978144248c
鸭脖yaboo <863176846@qq.com>
2022-05-22 17:10:46 +0800

    chore: pwa

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -16,7 +16,7 @@ module.exports = defineConfig({
   ],
   head: [
     ['link', { rel: 'icon', href: '/favicon.ico' }],
-    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['link', { rel: 'manifest', href: '/manifest.webmanifest' }],
     ['meta', { charset: 'UTF_8'}],
     ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1.0' }],
     ['meta', { name: 'theme-color', content: '#4c58bb' }],
